e9cd00c643f1a0bcf17665c3dad1fecebe26660a
Ilyass Gouriny <2131685@champlaincollege.qc.ca>
2024-02-07 13:22:28 -0500

    initial commit

---
+++ src/main/java/com/gouriny/fitnesscenterws/FitnesscenterWsApplication.java
@@ -0,0 +1,13 @@
+package com.gouriny.fitnesscenterws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnesscenterWsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnesscenterWsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/gouriny/fitnesscenterws/FitnesscenterWsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gouriny.fitnesscenterws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnesscenterWsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
